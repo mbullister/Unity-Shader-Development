4007b2e4b051b04eb5d2db6d0c72072cbf587c23
mbullister <mbullister@gmail.com>
2016-05-27 16:50:51 -0700

    DDS trilinear fix
    required for .dds cubemap support

---
+++ Editor/DDSfix.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+class DDSForceTrilinear : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        if (!assetPath.ToLower().EndsWith(".dds"))
+            return;
+
+        texture.filterMode = FilterMode.Trilinear;
+    }
+}
